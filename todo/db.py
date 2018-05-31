7f294120ce5320a53e4d607df6c15351d0487fc9
dameng <pingf0@gmail.com>
2018-05-31 20:00:57 +0800

    make conn close in the api handler

--- todo/db.py
+++ todo/db.py
@@ -50,4 +50,3 @@ if __name__ == '__main__':
     db = TodoDB()
     db.read_all()
 
-    # select * from todo;
--- todo/main.py
+++ todo/main.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 def index():
     db = TodoDB()
     todo = db.read_all()
+    db.close()
     return render_template('index.html', data=todo)
 
 @app.route('/todo/<int:todo_id>', methods=['DELETE'])
 def delete(todo_id):
     db = TodoDB()
     todo = db.delete(todo_id)
+    db.close()
     return 'okay'
 
 if __name__ == '__main__':
