174081070eadb0032bc240fc7e3e63824b0b19ca
dameng <pingf0@gmail.com>
2018-05-31 19:48:31 +0800

    make delete method works

--- todo/db.py
+++ todo/db.py
@@ -1,18 +1,39 @@
 import sqlite3
 
+#创立链接
+#获取游标
+#通过游标操作
+#.......
+#关闭游标
+#commit
+#关闭链接
 
 class TodoDB:
+    def __init__(self):
+        self.conn = sqlite3.connect('test.db')
+
+    def cursor(self):
+        return self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def commit(self):
+        self.conn.commit()
+
     def read_all(self):
-        conn = sqlite3.connect('test.db')
-        cursor = conn.cursor()
-        # 执行一条SQL语句，创建user表:
-        # insert into todo(content) values ('test1');
+        cursor = self.cursor()
         cursor = cursor.execute('select id, content from todo')
         data = cursor.fetchall()
         cursor.close()
-        conn.close()
-        # data = [d[0] for d in data]
         return data
+
+    def delete(self, todo_id):
+        cursor = self.cursor()
+        cursor = cursor.execute('delete from todo where id= ?', (todo_id, ))
+        cursor.close()
+        self.commit()
+        return
 
     def init_db(self):
         conn = sqlite3.connect('test.db')
@@ -22,11 +43,7 @@ class TodoDB:
         conn.commit()
         conn.close()
 
-    def test(self):
-        pass
 
-    def delete(self, todo_id):
-        print('delete: ', todo_id)
 
 
 if __name__ == '__main__':
